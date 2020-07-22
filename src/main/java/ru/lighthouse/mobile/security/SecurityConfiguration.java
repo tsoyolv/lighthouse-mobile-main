a0f9d4b47102ee85f98bc5cd33980f765bbc5fe2
tsoyolv <tsoyolv@gmail.com>
2020-07-22 15:38:49 +0300

    Fixed "origin has been blocked by CORS policy"

--- src/main/java/ru/lighthouse/mobile/security/SecurityConfiguration.java
+++ src/main/java/ru/lighthouse/mobile/security/SecurityConfiguration.java
@@ -1,17 +1,24 @@
 package ru.lighthouse.mobile.security;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import ru.lighthouse.mobile.security.jwt.JWTSecurityContextRepository;
 import ru.lighthouse.mobile.security.jwt.JWTService;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
+
+import java.util.Collections;
+import java.util.List;
 
 import static ru.lighthouse.mobile.App.HEALTH_URI;
 import static ru.lighthouse.mobile.SwaggerConfig.SWAGGER_URIES;
@@ -36,6 +43,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .formLogin().disable()
                 .httpBasic().disable()
@@ -53,6 +61,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(final WebSecurity webSecurity) {
         webSecurity.ignoring().antMatchers(SWAGGER_URIES);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     private SecurityContextRepository securityContextRepositoryObject() {
