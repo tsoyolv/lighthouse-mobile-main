d225b352c51149e5dd688d38cc964eac18176d6e
tsoyolv <tsoyolv@gmail.com>
2021-03-16 18:25:26 +0300

    Swagger security

---
+++ src/main/java/ru/lighthouse/mobile/main/boot/swagger/SwaggerSecurityConfig.java
@@ -0,0 +1,42 @@
+package ru.lighthouse.mobile.main.boot.swagger;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static ru.lighthouse.mobile.main.boot.swagger.SwaggerConfig.SWAGGER_URIES;
+
+// TODO doesn't work normally
+@ConditionalOnExpression("${security.enabled} && ${swagger.enabled}")
+@Configuration
+@Order(2)
+public class SwaggerSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("admin")
+            .password(passwordEncoder().encode("admin"))
+            .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers(SWAGGER_URIES)
+            .authenticated()
+            .and()
+            .httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
