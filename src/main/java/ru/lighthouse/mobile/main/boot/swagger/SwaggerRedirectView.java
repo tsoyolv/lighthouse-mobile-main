04206682cd162b13944f014c1caaa87b320960ed
tsoyolv <oleg.tcoi.work@gmail.com>
2021-04-29 18:13:04 +0300

    fixe SwaggerRedirectView

--- src/main/java/ru/lighthouse/mobile/main/boot/swagger/SwaggerRedirectView.java
+++ src/main/java/ru/lighthouse/mobile/main/boot/swagger/SwaggerRedirectView.java
@@ -1,17 +1,22 @@
 package ru.lighthouse.mobile.main.boot.swagger;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.view.RedirectView;
+import ru.lighthouse.mobile.main.boot.property.DomainProperties;
 
 @ConditionalOnExpression("${swagger.enabled}")
 @Controller
+@RequiredArgsConstructor
 public class SwaggerRedirectView {
+
+    private final DomainProperties domainProperties;
 
     @GetMapping(value = SwaggerConfig.URI)
     public RedirectView swaggerUi() {
-        return new RedirectView(SwaggerConfig.UI_HTML);
+        return new RedirectView(domainProperties.getUrl() + "/" + domainProperties.getServiceContextPath() + SwaggerConfig.UI_HTML);
     }
 }
